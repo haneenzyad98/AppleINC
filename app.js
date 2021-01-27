f53e474bbd0febcbb2f165ffc771bfe30b5de5ad
haneenzyad98 <haneenzyad98@gmail.com>
2021-01-27 13:47:35 +0200

    to creat good work

---
+++ app.js
@@ -0,0 +1,18 @@
+alert('Hello in Appel')
+
+var yourname = prompt('whats you name?');
+alert('hello ' + yourname);
+
+var yourAge = prompt('put your age hear');
+console.log * (yourAge);
+
+
+if (yourAge <= 13) {
+    massage = ' Tell Your parents to help you';
+} else if (yourAge > 14) {
+    massage = 'Read abut us';
+} else {
+    massage = 'Ok';
+}
+document.write('<h1>' + massage + '<h1>');
+
